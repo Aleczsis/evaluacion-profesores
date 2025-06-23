445f0b8d94adebfe50c7d1e06f47d96b0ac6cc8b
ana-cecilia03 <andceciliarodriguezalbarran7@gmail.com>
2025-06-23 11:47:44 -0600

    mensajes de error

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '148647',  // Cambia aquí tu contraseña
+  password: 'Ana_123@2025',  // Cambia aquí tu contraseña
   database: 'evaluacion_docente',
   waitForConnections: true,
   connectionLimit: 10,
---
+++ frontend/src/components/MensajesError.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-if="mensaje" class="mensaje-error">
+    <div class="mensaje-contenedor">
+      <span class="mensaje-texto">{{ mensaje }}</span>
+      <span class="mensaje-cerrar" @click="cerrarMensaje">×</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MensajesError',
+  props: {
+    mensaje: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    cerrarMensaje() {
+      this.$emit('cerrar')  // Emitimos el evento para manejarlo en el componente padre
+    }
+  }
+}
+</script>
+
+<style scoped>
+@import '@/assets/MensajesError.css';
+</style>
