62c132c76864a43c25f523fa7e8376bb7f18e958
Aleczsis <alexis1777.najera@gmail.com>
2025-06-24 16:55:55 -0600

    Cambie usuario y contarseña de BD

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -2,8 +2,8 @@ const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
   host: '100.106.39.85',     // IP del servidor remoto
-  user: 'usuario',           // Usuario MySQL remoto
-  password: '148647',        // Contraseña del usuario
+  user: 'ana',           // Usuario MySQL remoto
+  password: '12345',        // Contraseña del usuario
   database: 'evaluacion_docente',
   waitForConnections: true,
   connectionLimit: 10,
